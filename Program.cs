863474c332f36b5a7bbbbed6d988bcd005bcf916
Your Name <you@example.com>
2022-09-14 10:45:51 -0300

    Muestro INFO y DEBUG

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NLog;
+
+Logger log = LogManager.GetCurrentClassLogger();
+
+log.Info("Hola, esto es un programa de consola");
+log.Debug("Esto es un mensaje de debugueo");
